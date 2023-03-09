9da648c6c27c8461210132c21151216d7c40369f
ocetot <perisjara@gmail.com>
2023-03-09 20:31:33 -0300

    fin de la clase

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # react_clase_04
 
+* Ejercicio
+
+* Completar la class buton
+
+* Que se dibuje un boton en html luego de llamar al render.
+
+* Van a crear otra class input/text o el que sea y que se dibuje igual que boton en el html.
+
 
 ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
 ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
--- app.js
+++ app.js
@@ -1,20 +1,49 @@
-let myDom = document;
+let myDom = document; //document es un objeto global
+
+let name = "Firlais";
 
 const myDog = {
     name: "Firulais"
 };
 
 const myCat = {
-    "name" : "Kity",
+    "name": "Kity"
 };
-console.log(myDog);
-console.log(myCat);
 
+console.log(myDog.name);
+console.log(myCat.name);
+
+/*
 class Mouse {
-    constructor(name,age){
+    constructor(name, age) {
         this.name = name;
         this.age = age;
+    }
+}*/
 
+
+function Mouse(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+const myMouse = new Mouse("Perez", 4);
+
+class Button {
+    constructor(parentID, text) {
+        this.parentID = parentID;
+        // completa this.text
     }
 
+    render(){
+        let myApp = document.getElementById(this.parentID);
+
+        myApp.innerHTML = ""; // VAmos a escribir el html para que se 
+        // dibuje un boton con el texto de this.text
+    }
 }
+
+// Construir el objeto boton
+// y llamar al metodo render de ese objeto.
+// let myButon = new ....
+// myButon.metodoQueSea()
