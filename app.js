9b9c1a29d0c2ec6ffe2ae01d4c4ff579fb8e65b2
ocetot <perisjara@gmail.com>
2023-03-09 20:09:08 -0300

    primeros pasos

---
+++ app.js
@@ -0,0 +1,20 @@
+let myDom = document;
+
+const myDog = {
+    name: "Firulais"
+};
+
+const myCat = {
+    "name" : "Kity",
+};
+console.log(myDog);
+console.log(myCat);
+
+class Mouse {
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+
+    }
+
+}
