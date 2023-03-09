3794c2dfd7b35172fefe54b78db02314d5e753de
ocetot <perisjara@gmail.com>
2023-03-09 20:44:30 -0300

    oh

--- app.js
+++ app.js
@@ -31,18 +31,48 @@ const myMouse = new Mouse("Perez", 4);
 
 class Button {
     constructor(parentID, text) {
-        this.parentID = parentID;
-        // completa this.text
+      this.parentID = parentID;
+      this.text = text;
     }
 
-    render(){
-        let myApp = document.getElementById(this.parentID);
-
-        myApp.innerHTML = ""; // VAmos a escribir el html para que se 
-        // dibuje un boton con el texto de this.text
+    render() {
+      let myApp = document.getElementById(this.parentID);
+      let button = document.createElement("button");
+      button.innerText = this.text;
+      button.addEventListener("click", () => {
+        console.log("¡Haz hecho clic en el botón!");
+      });
+      myApp.appendChild(button);
     }
-}
+  }
 
+  class TextInput {
+    constructor(parentID, label) {
+      this.parentID = parentID;
+      this.label = label;
+    }
+
+    render() {
+      let myApp = document.getElementById(this.parentID);
+      let label = document.createElement("label");
+      label.innerText = this.label;
+      let input = document.createElement("input");
+      input.type = "text";
+      input.addEventListener("input", (event) => {
+        console.log(`El usuario ha escrito ${event.target.value}`);
+      });
+      myApp.appendChild(label);
+      myApp.appendChild(input);
+    }
+  }
+
+  // Crear un nuevo botón y agregarlo a la página
+  let myButton = new Button("my-app", "Click me!");
+  myButton.render();
+
+  // Crear una nueva entrada de texto y agregarla a la página
+  let myInput = new TextInput("my-app", "Nombre:");
+  myInput.render();
 // Construir el objeto boton
 // y llamar al metodo render de ese objeto.
 // let myButon = new ....
